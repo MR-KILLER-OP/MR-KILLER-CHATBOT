9aff615b208203563ba976e16e2d2d3f4601c5dd
Yewsdhi <126657493+Yewsdhi@users.noreply.github.com>
2024-12-12 08:09:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "81719734c6a0af15e5d35006655c1f84"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "6955568347"))
+OWNER_ID = int(getenv("OWNER_ID", "1384525218"))
 SUPPORT_GRP = "DNS_NETWORK"
-UPDATE_CHNL = "Dns_Official_Channel"
-OWNER_USERNAME = "II_RAJPUT_SHIV_OP_II"
+UPDATE_CHNL = "https://t.me/royalmusibotl"
+OWNER_USERNAME = "@II_ROYALENTRY1128_II"
