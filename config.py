37b9d277d4ad031c5752acfe0e095f0acf36c5c2
Md hakeeb <Alyabots636@gmail.com>
2025-04-20 22:17:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,7 +10,7 @@ API_HASH = "81719734c6a0af15e5d35006655c1f84"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "1384525218"))
-SUPPORT_GRP = "https://t.me/+PzhxPNaC_1Q4YTRl"
-UPDATE_CHNL = "https://t.me/DOSTON_KI_DUNIYAA"
-OWNER_USERNAME = "@II_ROYALENTRY1128_II"
+OWNER_ID = int(getenv("OWNER_ID", "7800109360"))
+SUPPORT_GRP = "https://t.me/DPZ_WORLD_OP"
+UPDATE_CHNL = "https://t.me/DPZ_WORLD_OP"
+OWNER_USERNAME = "@llMR_KILLERll"
